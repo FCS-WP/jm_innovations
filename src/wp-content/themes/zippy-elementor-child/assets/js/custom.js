6381e918721a5f667038bbe7af9068222527de20
Lucass1611 <chuong.nguyenquoc@floatingcube.com>
2025-10-03 22:38:43 +0700

    Lucas: Inital Flow Buy Ticket

--- src/wp-content/themes/zippy-elementor-child/assets/js/custom.js
+++ src/wp-content/themes/zippy-elementor-child/assets/js/custom.js
@@ -1,1 +1,46 @@
 $("#shop-block").find(".button").html("View Detail");
+$(".form-select");
+jQuery(document).ready(function ($) {
+  function calculateTotal() {
+    let package_price =
+      parseFloat(
+        $(".woocommerce-Price-amount bdi")
+          .clone()
+          .children()
+          .remove()
+          .end()
+          .text()
+          .trim()
+      ) || 0;
+    let package_price_final = package_price * 10000;
+    addon_price = parseFloat($());
+    console.log(package_price_final);
+  }
+  $("#package").on("change", function () {
+    calculateTotal();
+  });
+  $(".form-select").on("change", function () {
+    let selected = $(this).find(":selected");
+    let id = selected.data("id");
+    if (id) {
+      $(".add-ons-price-value").html(selected.data("price"));
+    }
+    calculateTotal();
+  });
+  $(".quantity").before("<div><label>Total:</label></div>");
+  $;
+  $("#flexCheckDefault").on("change", function () {
+    if ($(this).prop("checked")) {
+      $(".additinal-adult-select-block").css({
+        "pointer-events": "all",
+        "opacity": "1",
+      });
+    } else {
+      $(".additinal-adult-select-block").css({
+        "pointer-events": "none",
+        "opacity": "0.5",
+      });
+      $("#additional_adult_select").val("");
+    }
+  });
+});
---
+++ src/wp-content/themes/zippy-elementor-child/includes/zippy_custom_order.php
@@ -0,0 +1,18 @@
+<?php
+add_action('woocommerce_after_order_itemmeta', 'display_sub_products_in_admin_order', 10, 3);
+function display_sub_products_in_admin_order($item_id, $item, $product)
+{
+    $meta_data = $item->get_meta_data();
+}
+
+add_action('woocommerce_checkout_create_order_line_item', 'add_selected_sub_products_to_order_item', 10, 4);
+function add_selected_sub_products_to_order_item($item, $cart_item_key, $values, $order)
+{
+    if (!empty($values['add_on_id'])) {
+        $addon_product = wc_get_product($values['add_on_id']);
+        $item->add_meta_data('Add on', $addon_product->get_name(), true);
+    }
+    if (!empty($values['additional_adult_select'])) {
+        $item->add_meta_data('Additonal Adult', $values['additional_adult_select'], true);
+    }
+}
